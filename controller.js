6f2bdbea3d5b7364dc08e972f35ce33fe4cbcf95
Benjamin Lamptey <kbenlamptey@gmail.com>
2024-12-13 20:06:27 +0000

    this is the CRUD update and retrieve and added create authorModel for author details

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 const BookModel = require("./model");
+const AuthorModel = require("./author");
 
 ///create book
 const createBookcontroller = (req, res) => {
@@ -34,34 +35,81 @@ const retrieveBookcontroller = (req, res) => {
   }
 };
 
-// const updateBookcontroller = (req, res) => {
-//   const { id, title, author, description } = req.body;
-//   BookModel.findById(id).then((book) => {
-//     if (book) {
-//       book.title = title;
-//       book.author = author;
-//       book.description = description;
-//       book.save().then((result) => {
-//         res.json({ message: "book updated", data: result });
-//       });
-//     }
-//   });
-// };
+const updateBookcontroller = (req, res) => {
+  const { id, title, author, description } = req.body;
+  BookModel.findById(id).then((book) => {
+    if (book) {
+      book.title = title;
+      book.author = author;
+      book.description = description;
+      book.save().then((result) => {
+        res.json({ message: "book updated", data: result });
+      });
+    }
+  });
+};
 
 // //delete book
-// const deleteBookcontroller = (req, res) => {
-//   const { id } = req.body;
-//   BookModel.findByIdAndDelete(id)
-//     .then((result) => {
-//       res.json({ message: "book deleted", data: result });
-//     })
-//     .catch((err) => console.log(err));
-// };
+const deleteBookcontroller = (req, res) => {
+  const { id } = req.body;
+  BookModel.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ message: "book deleted", data: result });
+    })
+    .catch((err) => console.log(err));
+};
+
+const createAuthorcontroller = (req, res) => {
+  const { name, email, country, bookId } = req.body;
+  const author = new AuthorModel({
+    name,
+    email,
+    country,
+    bookId,
+  });
+  author
+    .save()
+    .then((result) => {
+      if (result) {
+        res.json({ message: "Author created successfully", data: result });
+      } else {
+        res.json({ message: "Author creation failed", data: result });
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+const retrieveAuthorcontroller = (req, res) => {
+  const { _id } = req.params;
+  if (_id) {
+    ///retrieve a single book
+    AuthorModel.find({ _id })
+      //  ///populate the bookId field with the book details
+      .populate("bookId")
+      .then((author) => {
+        ///sort the books by title in ascending order
+        res.json({ message: "author retrieved", data: author });
+      })
+      .catch((err) => console.log(err));
+  } else {
+    ////retrieve all books
+    AuthorModel.find()
+      ///populate the bookId field with the book details
+      .populate("bookId", "title description -_id")
+      ///sort the books by title in ascending order
+      .then((authors) => {
+        res.json({ message: "authors retrieved", data: authors });
+      })
+      .catch((err) => console.log(err));
+  }
+};
 
 //export controllers
 module.exports = {
   createBookcontroller,
   retrieveBookcontroller,
-  // updateBookcontroller,
-  //deleteBookcontroller,
+  updateBookcontroller,
+  deleteBookcontroller,
+  createAuthorcontroller,
+  retrieveAuthorcontroller,
 };
